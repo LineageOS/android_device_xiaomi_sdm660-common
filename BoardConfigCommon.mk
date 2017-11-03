1be61d5776df0b8da2f4ae9a090da2ea871f574b
Chris Fries <cfries@google.com>
2017-11-03 11:08:45 -0500

    sdm660-common: Set printk.devkmsg=on on kernel cmdline to avoid ratelimit
    init and friends are getting ratelimted on /dev/kmsg.  Let's keep
    the logs by setting printk.devkmsg=on.
    
    Bug: 62952768
    Test: Check for init logs in kernel during boot
    Change-Id: I103e3cc1d73880af1d9a0cdc4dae05223eb4c427

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,6 +33,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
