1481d7c72d1568ba7e809343e2d773d89fc5e1c3
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    sdm660-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- sdm660.mk
+++ sdm660.mk
@@ -36,6 +36,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
