1d6d3c55b6e1d5d0e3e54d035e4641c572ed7a0d
Suresh Koleti <skoleti@codeaurora.org>
2018-12-28 14:44:41 +0530

    sdm660-common: IMS: decouple ims-ext-common from boot jars
    -- Include ims_ext_common permission xml.
    
    Change-Id: Ide3ecae57cc1909180c8d8f323aa31620e900888
    CRs-Fixed: 2373721

--- sdm660.mk
+++ sdm660.mk
@@ -387,6 +387,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
