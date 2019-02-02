a675ef379c96fc62a669378a8888146d0e6a1691
LuK1337 <priv.luk@gmail.com>
2019-02-02 19:34:23 +0100

    sdm660-common: Add debug IMS props to workaround CarrierConfig restrictions
    We support IMS features on carriers not listed in the CarrierConfig,
    so let's use the props to enable it everywhere.
    
    Change-Id: Id3ad71d522a22048f7ad86d89695ad262361632d
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- properties.mk
+++ properties.mk
@@ -129,6 +129,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1  \
+    persist.dbg.wfc_avail_ovr=1
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
