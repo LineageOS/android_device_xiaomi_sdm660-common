f388be3e27e18bda8bc99e73e7a0363f8cfe135e
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    sdm660-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -24,6 +24,9 @@
 # Inherit proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 COMMON_PATH := device/xiaomi/sdm660-common
 
 # Inherit properties
