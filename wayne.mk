cd5ba5fb3b1721df0d373c75d233afb867d128bf
dianlujitao <dianlujitao@lineageos.org>
2019-05-31 16:06:44 +0800

    wayne-common: Update IFAA for pie blobs
    Change-Id: I2801cc3633416a1552c07230b68cf1d83b484edc

--- wayne.mk
+++ wayne.mk
@@ -139,7 +139,6 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_wayne
 
 PRODUCT_PACKAGES += \
-    IFAAService \
     org.ifaa.android.manager
 
 PRODUCT_BOOT_JARS += \
