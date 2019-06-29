ed93546d982c49955049d3df08fb036eecc75186
Isaac Chen <tingyi364@gmail.com>
2019-06-29 13:39:25 +0200

    Revert "wayne: Build with VNDK=current"
    This reverts commit 92cea1314ce8f9817ea872332171216084f31673.
    
    Change-Id: I79c7e8b8cb3453912464573e2150dc1e9e26254f

--- wayne.mk
+++ wayne.mk
@@ -431,10 +431,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK
-PRODUCT_PACKAGES += \
-    vndk_package
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
