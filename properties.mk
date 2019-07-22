cba017be5f2fce551706a11895abc53bb5a0afdc
Ady Abraham <adyabr@google.com>
2019-07-22 13:11:45 -0700

    sdm660-common: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8

--- properties.mk
+++ properties.mk
@@ -102,6 +102,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
