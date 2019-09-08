f4bd192aa8c704dfbcce93b608b59aa26f534362
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    sdm660-common: Make shim for libcamera.sdm660.so
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
    Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv" referenced by "/vendor/lib/hw/camera.sdm660.so"
    
    Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee

--- sdm660.mk
+++ sdm660.mk
@@ -394,6 +394,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(COMMON_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcamera_sdm660_shim
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
