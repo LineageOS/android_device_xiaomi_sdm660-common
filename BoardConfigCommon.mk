6355ada0f8be255ace7101a4bd861acb7992b4f1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-16 19:02:36 +0000

    sdm660-common: Correct define libinit variable
    Fixes:
    
    15:55:35 error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//device/xiaomi/sdm660-common/init/libinit_sdm660"
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_sdm660
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sdm660
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm660
 
 # Keystore
