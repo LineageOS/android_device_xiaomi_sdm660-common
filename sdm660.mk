be1f21d9a693cf7d36e88648a5bf2144ef993768
Davide Garberi <dade.garberi@gmail.com>
2019-10-02 19:33:08 +0200

    sdm660-common: Switch to power 1.2
    Change-Id: I7eba0105280192e1f397dafed7c0c3211435600e

--- sdm660.mk
+++ sdm660.mk
@@ -333,7 +333,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
