982b6dd360aac6b4371812459c66b7f1cb536880
dianlujitao <dianlujitao@lineageos.org>
2019-10-09 19:54:50 +0800

    sdm660-common: Build new GNSS HAL
     * Remove unnecessary dependencies on liblbs_core.so
     * Hardcode vendor loc HIDL version to 3.0 to enable vendor HIDL when
       building w/o BSP.
    
    Change-Id: I01f661f1d1e038737801f5a9db88e3388d470f74

--- sdm660.mk
+++ sdm660.mk
@@ -235,11 +235,13 @@ PRODUCT_ODM_PROPERTIES += \
 
 # GPS / Location
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti \
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti \
+    libbatching \
+    libgeofencing \
     libgnss \
-    libgnsspps \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libwifi-hal-ctrl
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
