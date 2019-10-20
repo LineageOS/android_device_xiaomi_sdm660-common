e1f1448d2220d7e43f4038698b0334341ac5430c
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    sdm660-common: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- sdm660.mk
+++ sdm660.mk
@@ -383,13 +383,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
