977f7edaf772109e5f21fd4c37ee37d0569b1ca5
argraur <argraur@gmail.com>
2019-10-26 17:08:09 +0000

    sdm660-common: Build vendor variant of libdng_sdk for watermark
    10-26 17:49:35.063  6856  6856 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:911 LibMap() dlopen: dlopen failed: library "libdng_sdk.so" not found, on '/vendor/lib/camera/components/com.qti.node.watermark.so'
    
    Signed-off-by: argraur <argraur@gmail.com>
    Change-Id: Id5d4a73d29418bd3b7c0c9f92e4d5efd8be68c3b
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
