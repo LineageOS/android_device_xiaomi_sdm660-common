72fe212dbd2fb7a3c67078baec216eaabf6eaa60
LuK1337 <priv.luk@gmail.com>
2019-11-22 15:35:26 +0100

    sdm660-common: Exclude Snap overlays from RRO
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- sdm660.mk
+++ sdm660.mk
@@ -38,7 +38,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
