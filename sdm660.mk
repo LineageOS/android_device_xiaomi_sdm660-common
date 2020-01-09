b8ed87b4246c7c2c45ba21a56cf036e5ed85c33e
Arvind Kumar <karvin@codeaurora.org>
2020-01-09 12:08:57 +0530

    sdm660-common: Enable graphics mapper version 2.1
     Enable graphics mapper version 2.1
    
    Change-Id: Iad70754a980eadcfd68752c76e569b503d398fae

--- sdm660.mk
+++ sdm660.mk
@@ -197,7 +197,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
