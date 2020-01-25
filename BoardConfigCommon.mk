a0d8ea071482c6c0dc62ff1cdf115c9016ea6aa6
dianlujitao <dianlujitao@lineageos.org>
2020-01-25 11:58:28 +0000

    sdm660-common: Remove bind mount for /persist
    This reverts commit edf6b1d8bff72cd64b15eb522368b690a2500345.
    
    symlink is enough with P blobs
    
    Change-Id: Ibfbf225345dec92c48f65b42b6c7d4a90401d7a3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,13 +185,12 @@ TARGET_EXFAT_DRIVER := exfat
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware \
-    /vendor/bt_firmware:/bt_firmware
+    /vendor/bt_firmware:/bt_firmware \
+    /mnt/vendor/persist:/persist
 
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-
-BOARD_ROOT_EXTRA_FOLDERS := persist
 
 TARGET_USES_MKE2FS := true
 
