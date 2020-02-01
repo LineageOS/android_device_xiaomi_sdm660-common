5d639aa1bdf8cb642c23207b181e5a897011cedf
dianlujitao <dianlujitao@lineageos.org>
2020-02-01 23:38:29 +0800

    Revert "wayne: Delete goodix data if other slot is taking up space"
    This reverts commit 25b47ac236e89800524f57b8176fe91d30d87f2d.
    
    Change-Id: I34ef8d2ff4e5373719eb61b63b94087bbb411494

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -83,11 +83,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.goodix.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.goodix.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
