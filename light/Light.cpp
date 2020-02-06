f417401d5b9b0f8e7bd4c04781150394b7569163
dianlujitao <dianlujitao@lineageos.org>
2020-02-07 00:04:57 +0800

    sdm660-common: Support button backlight
    Change-Id: Id13a42976df428c95c19eae49e1c114f057510bb

--- light/Light.cpp
+++ light/Light.cpp
@@ -24,6 +24,7 @@
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <unistd.h>
 
 namespace {
 
@@ -35,6 +36,8 @@ namespace {
 #define LEDS(x) PPCAT(/sys/class/leds, x)
 #define LCD_ATTR(x) STRINGIFY(PPCAT(LEDS(lcd-backlight), x))
 #define WHITE_ATTR(x) STRINGIFY(PPCAT(LEDS(white), x))
+#define BUTTON_ATTR(x) STRINGIFY(PPCAT(LEDS(button-backlight), x))
+#define BUTTON1_ATTR(x) STRINGIFY(PPCAT(LEDS(button-backlight1), x))
 
 using ::android::base::ReadFileToString;
 using ::android::base::WriteStringToFile;
@@ -125,6 +128,24 @@ Light::Light() {
         max_led_brightness_ = kDefaultMaxLedBrightness;
         LOG(ERROR) << "Failed to read max LED brightness, fallback to " << kDefaultMaxLedBrightness;
     }
+
+    if (!access(BUTTON_ATTR(brightness), W_OK)) {
+        lights_.emplace(std::make_pair(Type::BUTTONS,
+                                       [this](auto&&... args) { setLightButtons(args...); }));
+        buttons_.emplace_back(BUTTON_ATTR(brightness));
+
+        if (!access(BUTTON1_ATTR(brightness), W_OK)) {
+            buttons_.emplace_back(BUTTON1_ATTR(brightness));
+        }
+
+        if (ReadFileToString(BUTTON_ATTR(max_brightness), &buf)) {
+            max_button_brightness_ = std::stoi(buf);
+        } else {
+            max_button_brightness_ = kDefaultMaxLedBrightness;
+            LOG(ERROR) << "Failed to read max button brightness, fallback to "
+                       << kDefaultMaxLedBrightness;
+        }
+    }
 }
 
 Return<Status> Light::setLight(Type type, const LightState& state) {
@@ -152,6 +173,13 @@ Return<void> Light::getSupportedTypes(getSupportedTypes_cb _hidl_cb) {
 void Light::setLightBacklight(Type /*type*/, const LightState& state) {
     uint32_t brightness = RgbaToBrightness(state.color, max_screen_brightness_);
     WriteToFile(LCD_ATTR(brightness), brightness);
+}
+
+void Light::setLightButtons(Type /*type*/, const LightState& state) {
+    uint32_t brightness = RgbaToBrightness(state.color, max_button_brightness_);
+    for (auto&& button : buttons_) {
+        WriteToFile(button, brightness);
+    }
 }
 
 void Light::setLightNotification(Type type, const LightState& state) {
