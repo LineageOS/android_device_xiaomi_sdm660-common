b23e05ff626deaec0a8a8a839c46e047c5799911
Akhil Narang <akhilnarang.1999@gmail.com>
2020-04-27 21:38:48 -0300

    sdm660-common: add in whyred support
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Change-Id: I6844feb7d7b50c4ee1508211fa8b346c1aa11a2a

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout jason lavender twolip wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout jason lavender twolip wayne whyred,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 
