874001e92d88535f33f4ebe7aaa802a994eb95aa
dianlujitao <dianlujitao@lineageos.org>
2020-06-23 12:04:45 +0800

    sdm660-common: Remove keymap of headset buttons
    Change-Id: Ib64eb55a74e9ddfe53a0c0f8310cc0360c178d56

--- sdm660.mk
+++ sdm660.mk
@@ -312,7 +312,6 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
