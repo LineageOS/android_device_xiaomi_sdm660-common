4b6916e3e6a4d198cbf22d4e39cc2b43ffaf4592
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-10 12:10:21 -0400

    sdm660-common: bluetooth: Disable clean turn on
    * This isn't needed anymore.
    
    Change-Id: Ib77e5212099d82bbc07aea6044eb35109faff5cb

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,8 +24,5 @@
 #define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
 #define GATT_MAX_PHY_CHANNEL  10
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
-
 #define AVDT_NUM_SEPS 35
 #endif
