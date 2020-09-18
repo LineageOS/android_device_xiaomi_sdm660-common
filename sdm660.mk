7c28668c5e0718ec0910c816e6afc1eb52205548
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    sdm660-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- sdm660.mk
+++ sdm660.mk
@@ -276,7 +276,11 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # IDC
 PRODUCT_COPY_FILES += \
