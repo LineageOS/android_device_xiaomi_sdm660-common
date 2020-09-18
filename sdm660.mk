74a27972fd40a40b4f65a3de35d31e6de1fff262
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    sdm660-common: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- sdm660.mk
+++ sdm660.mk
@@ -448,12 +448,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
-
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
