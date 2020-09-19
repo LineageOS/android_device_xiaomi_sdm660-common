ad0cc5066ad579ec5312b52aca679991dc37dde0
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    sdm660-common: Import QCOM bluetooth soong namespace
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84

--- sdm660.mk
+++ sdm660.mk
@@ -34,7 +34,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Permissions
 PRODUCT_COPY_FILES += \
