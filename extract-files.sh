29e22ac118bffb4b905c322c63900bb541486cdb
Michael Bestas <mkbestas@lineageos.org>
2020-11-05 19:50:53 +0200

    sdm660-common: Switch to patchelf from tools-lineage
    Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

--- extract-files.sh
+++ extract-files.sh
@@ -63,11 +63,11 @@ function blob_fixup() {
     case "${1}" in
 
     vendor/bin/mlipayd@1.1)
-        patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+        "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;
 
     vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
-        patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+        "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
         ;;
     esac
