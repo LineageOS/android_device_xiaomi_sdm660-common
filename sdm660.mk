e03ac80cbe0bca9a3e730dabdb5a2a474a44d6d5
Michael Bestas <mkbestas@lineageos.org>
2020-11-05 17:03:26 +0200

    Revert "sdm660-common: Exclude lineage-sdk overlays from RRO"
    This reverts commit 5c4622d9dfee8528c87401fd6f7e56ddb244c543.
    
    Change-Id: Ie764de9b4624fb62cf4689bf0bea3854301f97c2

--- sdm660.mk
+++ sdm660.mk
@@ -33,7 +33,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay-system
 
 # Soong namespaces
