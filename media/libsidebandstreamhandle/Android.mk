56d05e432a98ba6f45df4d04a5c98a2ff7bc5bf7
Arne Coucheron <arco68@gmail.com>
2020-11-15 01:51:38 +0100

    sdm660-comon: media: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.
    
    Change-Id: Ic32a979a608bad3831bc0a2c64942f80c5028b01
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- media/libsidebandstreamhandle/Android.mk
+++ media/libsidebandstreamhandle/Android.mk
@@ -6,7 +6,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_CFLAGS :=
 
-LOCAL_C_INCLUDES:= $(TARGET_OUT_HEADERS)/qcom/display
+LOCAL_HEADER_LIBRARIES := display_headers
 
 LOCAL_SHARED_LIBRARIES := \
   libcutils \
