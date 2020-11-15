7dd75a74fd3ab953f178ed9c281b752fa143b662
Arne Coucheron <arco68@gmail.com>
2020-11-15 01:51:38 +0100

    sdm660-comon: media: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.
    
    Change-Id: Ic32a979a608bad3831bc0a2c64942f80c5028b01
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- media/mm-video-v4l2/vidc/venc/Android.mk
+++ media/mm-video-v4l2/vidc/venc/Android.mk
@@ -108,7 +108,6 @@ libmm-venc-inc      += $(LIBION_HEADER_PATHS)
 libmm-venc-inc      += $(call project-path-for,qcom-media)/mm-video-v4l2/vidc/common/inc
 libmm-venc-inc      += $(call project-path-for,qcom-media)/mm-core/inc
 libmm-venc-inc      += $(call project-path-for,qcom-media)/libstagefrighthw
-libmm-venc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-venc-inc      += $(TARGET_OUT_HEADERS)/adreno
 libmm-venc-inc      += $(call project-path-for,qcom-media)/libc2dcolorconvert
 libmm-venc-inc      += $(call project-path-for,qcom-media)/hypv-intercept
@@ -141,6 +140,7 @@ LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-venc-def)
 
 LOCAL_HEADER_LIBRARIES := \
+        display_headers \
         media_plugin_headers \
         libnativebase_headers \
         libcutils_headers \
@@ -182,6 +182,7 @@ LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-venc-def)
 
 LOCAL_HEADER_LIBRARIES := \
+        display_headers \
         media_plugin_headers \
         libnativebase_headers \
         libutils_headers \
