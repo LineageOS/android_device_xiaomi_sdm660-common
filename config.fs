80475962c9424f224a595e4e9f6fb4737ff0f958
Michael Bestas <mkbestas@lineageos.org>
2020-12-27 18:29:11 +0200

    sdm660-common: Remove unused entries from config.fs
    Change-Id: Id4bc301454f9ac675a296a1c74581211789711fc

--- config.fs
+++ config.fs
@@ -19,18 +19,6 @@ value:2906
 [AID_VENDOR_THERMAL]
 value:2907
 
-[vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
-[system/vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
@@ -42,24 +30,6 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
-
-[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND NET_ADMIN
-
-[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: BLOCK_SUSPEND NET_ADMIN
-
-[system/bin/cnss-daemon]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: NET_BIND_SERVICE
 
 [vendor/bin/pm-service]
 mode: 0755
@@ -73,12 +43,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [vendor/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
@@ -86,12 +50,6 @@ group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -168,12 +126,6 @@ mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
-
-[vendor/bin/xtwifi-client]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/sensors.qti]
 mode: 0755
