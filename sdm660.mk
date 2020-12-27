9a6ef9f19d70b8fecb1b0b01375ca09af0be0b9a
Michael Bestas <mkbestas@lineageos.org>
2020-12-27 15:56:42 +0200

    sdm660-common: Remove libantradio from PRODUCT_PACKAGES
    * It's proprietary and already included in vendor makefiles.
    
    Change-Id: Ic19703a39e3e691a04b226b2cb2a27b02703724d

--- sdm660.mk
+++ sdm660.mk
@@ -125,8 +125,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService \
     antradio_app \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
