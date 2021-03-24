2e451ae617b74f2945c7d92faa02bc3c238dfc6f
Albert Wang <albertccwang@google.com>
2021-03-25 00:03:02 +0800

    sdm660-common: Update USB HAL to V1.3 implementation
    Supports to enable/disable USB data signaling
    
    Bug: 161414036
    Test: Pass USB V1.3 HIDL tests
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Change-Id: Iffe00f8753206fb66cd3ab96cae5aa5ad9c410cd
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -392,7 +392,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-qti \
+    android.hardware.usb@1.3-service-qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
