614306ce63de0d4f08515b304e110c4e8a46c987
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 20:36:05 +0200

    sdm660-common: Stop building android.hardware.broadcastradio HIDL impl
    * Nothing uses it and it isn't even in manifest
    
    Change-Id: I7e991b1c851b80a1590df20943cf8a964e03ab28

--- sdm660.mk
+++ sdm660.mk
@@ -195,9 +195,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
-
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
 PRODUCT_PACKAGES += \
