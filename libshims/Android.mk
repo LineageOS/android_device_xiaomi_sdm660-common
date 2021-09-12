79a0f23c0d68dabc62b4124685d166454b5669cc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 23:34:34 +0200

    sdm660-common: libshims: Move to Android.bp
    Change-Id: Ic38ffa02b2e1dca5c842c738f4f1bcc978f8b5bd

--- libshims/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera_sdm660_shim.cpp
-LOCAL_SHARED_LIBRARIES := libui libutils
-LOCAL_MODULE := libcamera_sdm660_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
