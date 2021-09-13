6eb972eaea681a7d18c3d2f4636dc23c6295d4a8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-13 14:17:24 +0200

    sdm660-common: Conditionally build IR HIDL
    Change-Id: I7ebde1c32e2a64c867b7ae26c3aa254432386df2

--- sdm660.mk
+++ sdm660.mk
@@ -141,6 +141,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/component-overrides-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
+# ConsumerIR
+ifeq ($(BOARD_HAVE_IR),true)
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-service.xiaomi_sdm660
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+endif
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
