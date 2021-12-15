8b1366eb88046e216d22f70fd9b515711621587b
LuK1337 <priv.luk@gmail.com>
2021-12-15 17:58:52 +0100

    sdm660-common: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.0.
    
    Change-Id: Iae740ef18505b7f2bb01fe54d45810e72fd521e9

--- sdm660.mk
+++ sdm660.mk
@@ -104,11 +104,7 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    AntHalService
 
 # Biometrics
 PRODUCT_PACKAGES += \
