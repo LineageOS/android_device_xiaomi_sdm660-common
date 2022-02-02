b2f68e8b12d24d55461ffe72fd66ab11a6ccdda2
ZVNexus <akoskovich@pm.me>
2022-02-02 14:36:49 +0530

    sdm660-common: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: Ia2cb9394fdc65148100032620aabc306f7b3c1e7

--- sdm660.mk
+++ sdm660.mk
@@ -347,10 +347,6 @@ PRODUCT_PACKAGES += \
     libjson \
     libjson.vendor
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
