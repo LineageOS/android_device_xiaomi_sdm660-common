d43528f013e6de7c2cbba9d6043269940e3860f7
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:38:45 -0500

    sdm660-common: Specify androidboot.boot_devices
    So that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I270c1899e4487ed5e7657038c4ed07e407476953

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
+    androidboot.boot_devices=soc/c0c4000.sdhci \
     androidboot.configfs=true \
     androidboot.hardware=qcom \
     androidboot.usbcontroller=a800000.dwc3 \
