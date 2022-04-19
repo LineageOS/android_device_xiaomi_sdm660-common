f6d9f61aa324a1f19393b9241e3c542514b89807
Nolen Johnson <johnsonnolen@gmail.com>
2022-04-18 20:14:03 -0400

    sdm660-common: Squashed removal of TetheringOverlay
    * This was used to disable BPF, which we now support.
    
    This is a squashed commit of:
    
        Revert "msm8998-common TetheringOverlay: Disable BPF"
    
        This reverts commit 226925bc48a2cb6e83fd5ff81f065856881f039a.
    
        Change-Id: Icdd884e65d3ad9ce2c8e577b4d34a3ae380e48a5
    
    Change-Id: I067e41645c5c39bd3a60e9462920474d56e2e6f8

--- sdm660.mk
+++ sdm660.mk
@@ -384,10 +384,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
