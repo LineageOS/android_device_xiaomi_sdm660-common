6fbcb09c187c27e35c50afa9eac28af959f671ad
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-05-17 18:37:08 +0430

    sdm660-common: Update RIL blobs from S62Pro sdm660_64-user 11 RKQ1.210406.002 1.015.00 release-keys
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I4bc868d707dbc7c782ebaf4fc18f08984817d636

--- sdm660.mk
+++ sdm660.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
@@ -152,9 +153,11 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libgralloc.qti \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libqdMetaData.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
@@ -194,7 +197,9 @@ endif
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
-    libqti_vndfwk_detect.vendor
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
 
 # GPS / Location
 PRODUCT_PACKAGES += \
@@ -324,8 +329,11 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.secure_element@1.2 \
     rild \
+    libavservices_minijail.vendor \
     librmnetctl \
     libprotobuf-cpp-full
 
