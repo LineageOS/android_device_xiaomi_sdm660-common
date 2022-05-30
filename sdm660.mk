975d7ff50499c3b387f424624f3cf571e0e6c495
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-05-30 13:02:11 +0430

    sdm660-common: Update ril packges
    * needed by /vendor/bin/hw/qcrild
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Iaf3de8f8b356ff80b1bec7f4dc9dbc792f84e809

--- sdm660.mk
+++ sdm660.mk
@@ -350,11 +350,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.1 \
-    android.hardware.radio.config@1.1.vendor \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.1 \
-    android.hardware.secure_element@1.1.vendor \
+    android.hardware.secure_element@1.2 \
+    android.hardware.secure_element@1.2.vendor \
     rild \
     libavservices_minijail.vendor \
     librmnetctl \
