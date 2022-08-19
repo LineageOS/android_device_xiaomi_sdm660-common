3aace4a19cfe9a2c9cc127be91bb09646f05c33f
Aaron Kling <webgeek1234@gmail.com>
2022-08-18 23:17:54 -0500

    sdm660-common: Remove references to a2dp module
    APM would fail to parse the config anyway:
    
      E DevicesFactoryHAL: loadAudioInterface couldn't load audio hw module audio.a2dp (No such file or directory)
      W DevicesFactoryHalHidl: The specified device name is not recognized: "a2dp"
      E AudioFlinger: loadHwModule() error -22 loading module a2dp
      W APM_AudioPolicyManager: could not open HW module a2dp
    
    Change-Id: Iaa1be881cfe8f8474cef0ba46e1b0a62b59e14be

--- sdm660.mk
+++ sdm660.mk
@@ -94,7 +94,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
