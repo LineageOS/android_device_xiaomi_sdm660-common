19b9feaeb0441a0482486ad4c5164961c3f6a2df
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 01:04:01 +0200

    sdm660-common: Move to common Xiaomi fingerprint HIDL
    Change-Id: I6c29a831e985ee440c92a98aad8d33f25b9e5b2d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -75,3 +75,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.fingerprint.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.fingerprint.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
