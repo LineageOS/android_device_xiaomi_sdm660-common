b28889f71bab1b6e01f21fb605fa4b71a93fdbdb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 00:35:05 +0200

    sdm660-common: Remove duplicated RenderScript impl from PRODUCT_PACKAGES
    Change-Id: I6150f9befcbe50689eeb3f6f6ea49e5307d2de84

--- sdm660.mk
+++ sdm660.mk
@@ -155,7 +155,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@2.0 \
     vendor.qti.hardware.display.allocator-service \
