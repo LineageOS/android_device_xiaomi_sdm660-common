a288e5d1087d689917e4f5a66f83677c98fa71d4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-12 23:41:08 +0200

    [DNM] sdm660-common: Disable LiveDisplay
    Change-Id: I7f09a9cb6643a7132e6fb8cdea72fac9962523ad

--- sdm660.mk
+++ sdm660.mk
@@ -276,7 +276,7 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
     vendor.lineage.livedisplay@2.0-service-sysfs
 
