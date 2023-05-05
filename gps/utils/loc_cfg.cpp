50bfd7be585ab187bcd7cd8d63f09f6e534005fb
Michael Bestas <mkbestas@gmail.com>
2023-05-05 20:09:13 +0300

    sdm660-common: Implement xtra-daemon control via property
    Default to false if the property is not set.
    Device init scripts will need to be adapted to restart
    loc_launcher service when property change is detected.
    
    Change-Id: Icbda789cd2a9a3391f437af2c07fc5127f2f09ab

--- gps/utils/loc_cfg.cpp
+++ gps/utils/loc_cfg.cpp
@@ -88,6 +88,12 @@ const char LOC_PATH_APDR_CONF[] = LOC_PATH_APDR_CONF_STR;
 const char LOC_PATH_XTWIFI_CONF[] = LOC_PATH_XTWIFI_CONF_STR;
 const char LOC_PATH_QUIPC_CONF[] = LOC_PATH_QUIPC_CONF_STR;
 
+bool isXtraDaemonEnabled() {
+    bool enabled = property_get_bool("persist.sys.xtra-daemon.enabled", false);
+    LOC_LOGe("xtra-daemon enabled: %d\n", enabled);
+    return enabled;
+}
+
 bool isVendorEnhanced() {
     return sVendorEnhanced;
 }
@@ -774,6 +780,13 @@ int loc_read_process_conf(const char* conf_file_name, uint32_t * process_count_p
            !baseband_length || !status_length || !auto_platform_length) {
             LOC_LOGE("%s:%d]: Error: i: %d; One of the parameters not specified in conf file",
                      __func__, __LINE__, i);
+            continue;
+        }
+
+        if (strcmp(conf.proc_name, "xtra-daemon") == 0 && !isXtraDaemonEnabled()) {
+            LOC_LOGE("%s:%d]: Process xtra-daemon is disabled via property",
+                     __func__, __LINE__);
+            child_proc[j].proc_status = DISABLED_FROM_CONF;
             continue;
         }
 
