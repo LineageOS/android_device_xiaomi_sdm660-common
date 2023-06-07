38c2eb9696240f14836308e540b1cde4a9c03a4c
Michael Bestas <mkbestas@gmail.com>
2023-06-07 23:28:37 +0300

    sdm660-common: gps: Remove all Wno-error flags
    Almost all warnings/errors have been fixed.
    Ignore undefined bool conversion error on purpose.
    
    Change-Id: I00cb5f38711fa9f873da030fc6a0ebc6cde3ceeb

--- gps/build/target_specific_features.mk
+++ gps/build/target_specific_features.mk
@@ -1,20 +1,6 @@
 GNSS_CFLAGS := \
     -Werror \
-    -Wno-error=unused-parameter \
-    -Wno-error=macro-redefined \
-    -Wno-error=reorder \
-    -Wno-reorder-ctor \
-    -Wno-error=missing-braces \
-    -Wno-error=self-assign \
-    -Wno-enum-conversion \
-    -Wno-error=logical-op-parentheses \
-    -Wno-error=null-arithmetic \
-    -Wno-error=null-conversion \
-    -Wno-error=parentheses-equality \
-    -Wno-undefined-bool-conversion \
-    -Wno-error=tautological-compare \
-    -Wno-error=switch \
-    -Wno-error=date-time
+    -Wno-undefined-bool-conversion
 
 LOCAL_SANITIZE := signed-integer-overflow, unsigned-integer-overflow
 
--- gps/pla/Android.mk
+++ gps/pla/Android.mk
@@ -1,20 +1,6 @@
 GNSS_CFLAGS := \
     -Werror \
-    -Wno-error=unused-parameter \
-    -Wno-error=macro-redefined \
-    -Wno-error=reorder \
-    -Wno-reorder-ctor \
-    -Wno-error=missing-braces \
-    -Wno-error=self-assign \
-    -Wno-enum-conversion \
-    -Wno-error=logical-op-parentheses \
-    -Wno-error=null-arithmetic \
-    -Wno-error=null-conversion \
-    -Wno-error=parentheses-equality \
-    -Wno-undefined-bool-conversion \
-    -Wno-error=tautological-compare \
-    -Wno-error=switch \
-    -Wno-error=date-time
+    -Wno-undefined-bool-conversion
 
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 ifneq ($(BUILD_TINY_ANDROID),true)
