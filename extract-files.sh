9c0db7f9f2ee71453e8956d23140df61e435df80
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    sdm660-common: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,9 @@ function blob_fixup() {
         vendor/bin/mlipayd@1.1)
            "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
             ;;
+        vendor/bin/pm-service)
+            grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+            ;;
         vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
             "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
             sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
