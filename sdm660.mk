7ff6145ecef812e49ca939642bc2e593991cded5
Bruno Martins <bgcngm@gmail.com>
2024-06-19 11:24:00 +0530

    sdm660-common: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- sdm660.mk
+++ sdm660.mk
@@ -410,10 +410,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
