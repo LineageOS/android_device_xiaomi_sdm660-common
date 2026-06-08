a398ab4ab5e8defa0a4596aded1b5aff51f3f204
Michael Bestas <mkbestas@lineageos.org>
2026-06-08 17:02:01 +0300

    Reapply "sdm660-common: Switch to Audio HAL V6"
    This reverts commit 0fd18be554c47b010c140627b7e3534ead2cf2f2.
    
    Reason for revert: Required for FCM level 5
    
    Change-Id: I20dca0736aae7614c50361611aaa07d16fee0418

--- sdm660.mk
+++ sdm660.mk
@@ -60,8 +60,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl:32 \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32
